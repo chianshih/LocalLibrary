03124c1d01216a59c8c258a7867812a682d27ce3
chianshih <chian.shih@gmail.com>
2023-01-11 13:33:42 +0800

    Generic list and detail views

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    # num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_books = Book.objects.filter(title__exact='Dune').count()
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    num_gernes = Genre.objects.filter(name__exact='Fantasy').count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_gernes': num_gernes,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location\
+        
+class BookDetailView(generic.DetailView):
+    model = Book
